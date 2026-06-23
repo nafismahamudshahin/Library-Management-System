911357188fd1aa4da244b9ad98a86ab25b0df58d
nafismahamudshahin <nafismahamudshahin@gmail.com>
2026-06-23 19:44:01 +0600

    create category and book model

--- books/models.py
+++ books/models.py
@@ -2,5 +2,17 @@ from django.db import models
 from members.models import Author
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
 
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author , on_delete=models.CASCADE, related_name='books')
+    isbn = models.CharField(max_length=13)
+    category = models.ForeignKey(Category , on_delete=models.CASCADE , related_name="books")
+    published_year = models.DateField(blank=True , null=True)
+    stock = models.PositiveIntegerField(blank=True)
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
