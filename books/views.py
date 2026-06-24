a5d3dd92938ec937f9a371abe3bb85128aa87e1e
nafismahamudshahin <nafismahamudshahin@gmail.com>
2026-06-25 03:44:30 +0600

    implement book modelviewset

--- books/views.py
+++ books/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from books.models import Book
+from books.serializers import BookSerializer
 # Create your views here.
+
+class BookModelViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
