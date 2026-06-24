8fa41ab4461d28b73bd2491270992bf62396d91b
nafismahamudshahin <nafismahamudshahin@gmail.com>
2026-06-25 03:33:15 +0600

    implement author modelviewset

--- members/views.py
+++ members/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from members.serializers import AuthorSerializer
+from members.models import Author
 # Create your views here.
+
+class AuthorModelViewSet(ModelViewSet):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
