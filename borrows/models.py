88a713dfe8d0addb3f4701518f5bd015e59856ca
nafismahamudshahin <nafismahamudshahin@gmail.com>
2026-06-24 23:15:07 +0600

    Modify Borrow Record Model fields

--- borrows/models.py
+++ borrows/models.py
@@ -3,12 +3,13 @@ from books.models import Book
 from members.models import Member
 # Create your models here.
 class BorrowRecord(models.Model):
-    book = models.ForeignKey(Book, on_delete=models.PROTECT , related_name="borrow_record")
+    book = models.ForeignKey(Book, on_delete=models.PROTECT , related_name="borrow_records")
     member = models.ForeignKey(Member, on_delete=models.CASCADE , related_name="borrowings")
     borrow_date = models.DateField(auto_now_add=True)
-    due_date = models.DateField(auto_now_add=True)
-    return_date = models.DateField()
-    is_returned = models.BooleanField()
-    is_created = models.Date()
+    due_date = models.DateField()
+    return_date = models.DateField(null=True,blank=True)
+    is_returned = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-
+    def __str__(self):
+        return f"{self.member.name} - {self.book.title}"
